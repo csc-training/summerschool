4460fedd33f53632dd0bf618303939d5b08ffae2
natj <nattila.joonas@gmail.com>
2017-04-28 09:33:59 +0300

    fix fortran: loops

--- exercises/programming/fortran/loops/loops.F90
+++ exercises/programming/fortran/loops/loops.F90
@@ -1,21 +1,21 @@
 program loops
   implicit none
-! TODO define parameters n and m
+! TODO define parameters nx and ny
 ! TODO: define floating point array A
   integer :: i, j
 
 
 ! TODO initialize array A here
-! Remember that now we have A(ydir, xdir) 
-! so we define height with n and width with m.
+! Remember that we now have A(ydir, xdir),
+! so we define height with ny and width with nx.
 
 
 
 
 !--------------------------------------------------
 ! printing of the final array
-  do i = 1, n
-     write(*,'(*(F4.1))') A(i,:)
+  do j = 1, ny
+     write(*,'(*(F4.1))') A(j,:)
   end do
 
 end program loops
--- solutions/programming/fortran/loops/loops.F90
+++ solutions/programming/fortran/loops/loops.F90
@@ -1,47 +1,47 @@
 program loops
   implicit none
-  integer, parameter :: n = 16, m = 16
-  real, dimension(n, m) :: A
+  integer, parameter :: nx = 16, ny = 16
+  real, dimension(ny, nx) :: A
   integer :: i, j
 
 
 ! Initialize everything first to zero 0.0
 ! Remember that now we have A(ydir, xdir) 
-! so we define height with n and width with m.
+! so we define height with ny and width with nx.
 
-! For clarity we associate i with y and j with x
-  do i = 2, n-1
-    do j = 2, m-1
-      A(i,j) = 0.0
+! For clarity we associate i with x and j with y
+  do i = 2, nx-1
+    do j = 2, ny-1
+      A(j,i) = 0.0
     end do
   end do
 
 ! Top
-  do j = 1, m
-    A(1,j) = 1.0
+  do i = 1, nx
+    A(1,i) = 1.0
   end do
 
 ! Bottom
-  do j = 1, m
-    A(n,j) = 2.0
+  do i = 1, nx
+    A(ny,i) = 2.0
   end do
 
 ! Left
-  do i = 2, n-1
-    A(i,1) = 3.0
+  do j = 2, ny-1
+    A(j,1) = 3.0
   end do
 
 ! Right
-  do i = 2, n-1
-    A(i,m) = 4.0
+  do j = 2, ny-1
+    A(j,nx) = 4.0
   end do
 
 
 
 !--------------------------------------------------
 ! printing of the final array
-  do i = 1, n
-     write(*,'(*(F4.1))') A(i,:)
+  do j = 1, ny
+     write(*,'(*(F4.1))') A(j,:)
   end do
 
 end program loops
