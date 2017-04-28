959ce145e80055e137adf3578c0901f738430274
natj <nattila.joonas@gmail.com>
2017-04-28 11:09:37 +0300

    fortran: array intrinsics

---
+++ solutions/programming/fortran/intrinsics/intrinsics.F90
@@ -0,0 +1,47 @@
+program intrinsics
+  implicit none
+  integer :: nx, ny
+  integer :: j, alloc_stat
+  real, dimension(:,:), allocatable :: A
+
+  write (*,*) 'Give x and y dimensions of matrix A:'
+  read (*,*) nx, ny
+
+  allocate(A(ny,nx), stat = alloc_stat)
+  if (alloc_stat /= 0) call abort()
+
+  ! initialize middle to zero
+  A(:,:)  = 0.0
+
+  ! first left and right
+  A(1,:)  = 3.0
+  A(nx,:) = 4.0
+
+  ! then top and bottom to get corners correct
+  A(:,1)  = 1.0
+  A(:,ny) = 2.0
+
+  !--------------------------------------------------
+  ! Print out the arrays
+  do j = 1, ny
+     write(*,'(*(F5.2))') A(:,j)
+  end do
+
+
+  !--------------------------------------------------
+  ! Using array intrinsics to get information from array A
+
+  write (*,*) 'a) Sum of elements across 2nd dimension of A: ', sum(A,2) 
+
+  write (*,*) 'b) Coordinates of maximum element: ', maxloc(A)
+
+  write (*,*) 'c) Absolute minimum value: ', minval(A) 
+
+  write (*,*) 'd) Are elements of A greater than or equal to 0: ', any(A >= 0.0)
+
+  write (*,*) 'e) Number of elements greater than or equal to 0.5: ', count(A >= 0.5)
+
+
+
+
+end program intrinsics
