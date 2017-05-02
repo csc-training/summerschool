d669cda30ae660c8ce4e2008bc75acf605ac8a9d
Jussi Enkovaara <jenkovaa@tilitantti.x.csc.fi>
2017-05-02 15:29:41 +0300

    Skeleton codes for C

---
+++ exercises/programming/C/datatypes_expressions/pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   int a, b, c;
+   // declare a pointer variable and assign it to address of e.g. a
+   
+   // Evaluate expressions using both the original and the pointer variable
+   // and investigate the value / value pointed to
+
+
+   return 0;
+}
