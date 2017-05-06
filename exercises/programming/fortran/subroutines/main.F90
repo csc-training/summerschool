7131ca7c529227e6707df1136c16c921c4e81f78
pmanninen <pmannin@csc.fi>
2017-05-06 15:26:20 +0300

    ex6 skeleton

---
+++ exercises/programming/fortran/subroutines/main.F90
@@ -0,0 +1,20 @@
+program subroutines
+  use laplacian_mod
+  implicit none
+! TODO: define the arrays
+  integer :: nx, ny, iter
+
+  write (*,*) 'Give x and y dimensions of a matrix:'
+  read (*,*) nx, ny
+
+  allocate(previous(nx,ny), current(nx,ny))
+  call initialize(previous)
+  call write_field(previous)
+
+  ! compute the Laplacian
+  call laplacian(previous, current)
+  ! print the result array
+  call write_field(current)
+ 
+end program subroutines
+
