a338606baa7596c3fb91884eabca1f7891fba11b
pmanninen <pmannin@csc.fi>
2017-05-06 15:38:44 +0300

    jyrays

--- solutions/programming/fortran/hello/hello.F90
+++ solutions/programming/fortran/hello/hello.F90
@@ -1,5 +1,7 @@
 program hello
-  implicit none
-
-  write (*,*) 'Hello world from Fortran!'
+  integer :: a = 1, b = 2
+  real :: c
+  c = a+b
+  write(*,*) 'Hello world!'
+  write(*,*) 'a=', a, 'b=', b, 'a+b=', c, 'sqrt(a+b)', sqrt(c)
 end program hello
