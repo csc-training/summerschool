3c3ba6ac9f1bc443cd3c145797d53163747dea58
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2017-06-02 13:30:32 +0300

    Skeletons for message chain with timing measurement

--- exercises/mpi/c/non-blocking/msg-chain.c
+++ exercises/mpi/c/non-blocking/msg-chain.c
@@ -6,11 +6,13 @@
 int main(int argc, char *argv[])
 {
     int i, myid, ntasks;
-    int size = 100;
+    int size = 10000000;
     int *message;
     int *receiveBuffer;
     MPI_Status statuses[2];
     MPI_Request requests[2];
+
+    double t0, t1;
 
     int source, destination;
     int count;
@@ -40,6 +42,10 @@ int main(int argc, char *argv[])
         source = MPI_PROC_NULL;
     }
 
+    /* Start measuring the time spend in communication */
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+
     /* Send and receive messages as defined in exercise */
     /* TODO: Implement the communication using non-blocking
              sends and receives */
@@ -54,6 +60,13 @@ int main(int argc, char *argv[])
     MPI_Get_count(&statuses[0], MPI_INT, &count);
     printf("Receiver: %d. received elements: %d. Tag %d. Sender: %d.\n",
            myid, count, statuses[0].MPI_TAG, statuses[0].MPI_SOURCE);
+
+    /* Finalize measuring the time and print it out */
+    t1 = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+    fflush(stdout);
+
+    printf("Time elapsed in rank %2d: %6.3f\n", myid, t1 - t0);
 
     free(message);
     free(receiveBuffer);
--- exercises/mpi/fortran/non-blocking/msg-chain.f90
+++ exercises/mpi/fortran/non-blocking/msg-chain.f90
@@ -1,14 +1,18 @@
 program basic
   use mpi
+  use iso_fortran_env, only : REAL64
+
   implicit none
   !  include 'mpif.h'
-  integer, parameter :: size = 100
+  integer, parameter :: size = 10000000
   integer :: rc, myid, ntasks, count
   integer :: status(MPI_STATUS_SIZE,2)
   integer :: message(size)
   integer :: receiveBuffer(size)
   integer :: source, destination
   integer :: requests(2)
+
+  real(REAL64) :: t0, t1 
 
   call MPI_INIT(rc)
   call MPI_COMM_RANK(MPI_COMM_WORLD, myid, rc)
@@ -29,6 +33,10 @@ program basic
      source = MPI_PROC_NULL
   end if
 
+  ! Start measuring the time spent in communication
+  call mpi_barrier(mpi_comm_world, rc)
+  t0 = mpi_wtime()
+
   ! TODO: Implement the message passing using non-blocking
   !       sends and receives
 
@@ -44,6 +52,13 @@ program basic
        'received elements: ', count, &
        '. Tag: ', status(MPI_TAG, 1), &
        '. Sender:   ', status(MPI_SOURCE, 1)
+
+  ! Finalize measuring the time and print it out
+  t1 = mpi_wtime()
+  call mpi_barrier(mpi_comm_world, rc)
+  call flush(6)
+
+  write(*, '(A20, I3, A, F6.3)') 'Time elapsed in rank', myid, ':', t1-t0
 
   call MPI_FINALIZE(rc)
 end program basic
