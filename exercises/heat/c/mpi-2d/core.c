0069cc5cd0cf923997bf19e29a60e36831e20939
Jussi Enkovaara <jenkovaa@tilitantti.x.csc.fi>
2017-06-20 14:59:32 +0300

    Skeletons for 2d heat equation solver

---
+++ exercises/heat/c/mpi-2d/core.c
@@ -0,0 +1,52 @@
+/* Main solver routines for heat equation solver */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+
+#include "heat.h"
+
+/* Exchange the boundary values */
+void exchange(field *temperature, parallel_data *parallel)
+{
+  /* TODO start: implement 2D halo exchange using MPI datatypes */
+
+    // Send to the up, receive from down
+
+    // Send to the down, receive from up
+
+    // Send to the left, receive from right
+
+    // Send to the right, receive from left
+
+  /* TODO end */
+}
+
+
+/* Update the temperature values using five-point stencil */
+void evolve(field *curr, field *prev, double a, double dt)
+{
+    int i, j;
+    double dx2, dy2;
+
+    /* Determine the temperature field at next time step
+     * As we have fixed boundary conditions, the outermost gridpoints
+     * are not updated. */
+    dx2 = prev->dx * prev->dx;
+    dy2 = prev->dy * prev->dy;
+    for (i = 1; i < curr->nx + 1; i++) {
+        for (j = 1; j < curr->ny + 1; j++) {
+            curr->data[i][j] = prev->data[i][j] + a * dt *
+                               ((prev->data[i + 1][j] -
+                                 2.0 * prev->data[i][j] +
+                                 prev->data[i - 1][j]) / dx2 +
+                                (prev->data[i][j + 1] -
+                                 2.0 * prev->data[i][j] +
+                                 prev->data[i][j - 1]) / dy2);
+        }
+    }
+}
+
+
