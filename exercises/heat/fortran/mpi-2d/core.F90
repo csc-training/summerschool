96e80d67cd0360bf5b14878267d00e95604a6452
Jussi Enkovaara <jenkovaa@tilitantti.x.csc.fi>
2017-06-20 14:59:32 +0300

    Skeletons for 2d heat equation solver

---
+++ exercises/heat/fortran/mpi-2d/core.F90
@@ -0,0 +1,60 @@
+! Main solver routines for heat equation solver
+module core
+    use heat
+
+contains
+
+  ! Exchange the boundary data between MPI tasks
+  subroutine exchange(field0, parallel)
+    use mpi
+
+    implicit none
+
+    type(field), intent(inout) :: field0
+    type(parallel_data), intent(in) :: parallel
+
+    integer :: ierr
+
+    ! TODO start: implement 2D halo exchange using MPI datatypes
+
+    ! Send to left, receive from right
+
+    ! Send to right, receive from left
+
+    ! Send to up receive from down
+
+    ! Send to the down, receive from up
+
+    ! TODO end
+
+  end subroutine exchange
+
+  ! Compute one time step of temperature evolution
+  ! Arguments:
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  !   a (real(dp)): update equation constant
+  !   dt (real(dp)): time step value
+  subroutine evolve(curr, prev, a, dt)
+
+    implicit none
+
+    type(field), intent(inout) :: curr, prev
+    real(dp) :: a, dt
+    integer :: i, j, nx, ny
+
+    nx = curr%nx
+    ny = curr%ny
+
+    do j = 1, ny
+       do i = 1, nx
+          curr%data(i, j) = prev%data(i, j) + a * dt * &
+               & ((prev%data(i-1, j) - 2.0 * prev%data(i, j) + &
+               &   prev%data(i+1, j)) / curr%dx**2 + &
+               &  (prev%data(i, j-1) - 2.0 * prev%data(i, j) + &
+               &   prev%data(i, j+1)) / curr%dy**2)
+       end do
+    end do
+  end subroutine evolve
+
+end module core
