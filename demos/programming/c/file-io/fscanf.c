364ce9b26ac2baaa40de8dc76ef9e03cf3d47be8
Sebastian von Alfthan <sebastian.von.alfthan@csc.fi>
2017-06-29 09:44:00 +0300

    fscanf example

--- demos/programming/c/file-io/fscanf.c
+++ demos/programming/c/file-io/fscanf.c
@@ -2,20 +2,17 @@
 
 int main(){
   FILE *fp;
-  char str[5];
-
+  int a,b,c;
+  int n;
+  
   /* opening file for reading */
   fp = fopen("file.txt" , "r");
   if(fp == NULL) {
     fprintf(stderr, "Error: file.txt did not exist\n");
-
     return(-1);
   }
-  fgets (str, 5, fp);
-  printf("%s", str);
-  //fputs is a simpler function that just writes 
-  //a string to the file
-  //fputs(str, stdout);
+  n=fscanf(fp, "%d\n%d\n%d\n",&a, &b, &c);
+  printf("Read %d numbers: %d %d %d\n", n, a, b, c);
 
   fclose(fp);
    
