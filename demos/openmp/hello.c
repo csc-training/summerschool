3be135c4eb38bb847970fc9b088e01c39c266c22
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-06-30 11:20:00 +0300

    Demo of execution controls

--- demos/openmp/hello.c
+++ demos/openmp/hello.c
@@ -2,10 +2,10 @@
 #include <omp.h>
 
 int main(int argc, char argv[]) {
-	int omp_rank;
+    int omp_rank;
 #pragma omp parallel private(omp_rank)
-	{
-		omp_rank = omp_get_thread_num();
-		printf("Hello world! by thread %d\n", omp_rank);
-	}
+    {
+        omp_rank = omp_get_thread_num();
+        printf("Hello world! by thread %d\n", omp_rank);
+    }
 }
