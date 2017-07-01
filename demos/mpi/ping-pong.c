bdad79caae04d3198a211dc2c0bd856314cfa132
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-07-01 13:39:50 +0300

    Ping-pong demo with send/recv

---
+++ demos/mpi/ping-pong.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char argv[]) {
+    int i, rank, size;
+    float data=42.0;
+    int tag=0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        for (i=1; i < size; i++) {
+            printf("[%d] Ping.\n", rank);
+            MPI_Send(data, 1, MPI_REAL, i, tag, MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv(&data, 1, MPI_REAL, 0, tag, MPI_COMM_WORLD);
+        printf("[%d] Pong.\n", rank);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
