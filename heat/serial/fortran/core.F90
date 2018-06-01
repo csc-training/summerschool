ad65deb2030dc6cbf99f010cb44327b473e26ebd
Pekka Manninen <pekka.manninen@csc.fi>
2018-06-01 18:42:00 +0300

    heat serial / fortran

--- heat/serial/fortran/core.F90
+++ heat/serial/fortran/core.F90
@@ -21,15 +21,7 @@ contains
     nx = curr%nx
     ny = curr%ny
 
-    do j = 1, ny
-       do i = 1, nx
-          curr%data(i, j) = prev%data(i, j) + a * dt * &
-               & ((prev%data(i-1, j) - 2.0 * prev%data(i, j) + &
-               &   prev%data(i+1, j)) / curr%dx**2 + &
-               &  (prev%data(i, j-1) - 2.0 * prev%data(i, j) + &
-               &   prev%data(i, j+1)) / curr%dy**2)
-       end do
-    end do
+! TODO: implement the heat equation update
   end subroutine evolve
 
 end module core
