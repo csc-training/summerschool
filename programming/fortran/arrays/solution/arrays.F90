c41ec8c776e90113e0c30af481b3c40d7450d14f
Pekka Manninen <pekka.manninen@csc.fi>
2018-06-01 13:32:34 +0300

    ex3

--- programming/fortran/arrays/solution/arrays.F90
+++ programming/fortran/arrays/solution/arrays.F90
@@ -1,28 +1,33 @@
 program arrays
   implicit none
-  integer :: nx, ny
-  integer :: i, alloc_stat
+! TODO: Define the array A
   real, dimension(:,:), allocatable :: A
+  real :: x, y, dx, dy
+  integer :: nx, ny, i, j, alloc_stat
 
   write (*,*) 'Give number of rows and columns for matrix A:'
   read (*,*) nx, ny
+  dx = 1.0/real(nx-1)
+  dy = 1.0/real(ny-1)
 
-  allocate(A(nx,ny), stat = alloc_stat)
-  if (alloc_stat /= 0) call abort()
+! TODO: allocate the array A
+  allocate (A(nx, ny), stat=alloc_stat)
+  if (alloc_stat /= 0) stop
 
-  ! Initializing array
-  A(:,:)  = 65.0 ! middle
-  A(:,1)  = 20.0 ! left
-  A(:,ny) = 70.0 ! right
-  A(1,:)  = 85.0 ! top
-  A(nx,:) = 5.0  ! bottom
-
-
-  !--------------------------------------------------
-  ! Print out the array
-  do i = 1, nx
-    write(*,'(*(F6.1))') A(i,:)
+! TODO: initalize the array A
+  y = 0.0
+  do j = 1, ny
+     x = 0.0
+     do i = 1, nx
+        A(i, j) = x**2 + y**2
+        x = x + dx
+     end do
+     y = y + dy
   end do
 
+! TODO: Print out the array
+  do i = 1, nx
+     write (*, '(*(F6.2))') A(i,:)
+  end do
 
 end program arrays
