4112185595780331eae102ea0c9bed406c070494
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-06-08 11:45:30 +0300

    Hybrid mpi+openacc hello world

--- hybrid-gpu/hello-world/hello.c
+++ hybrid-gpu/hello-world/hello.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #ifdef _OPENACC
 #include <openacc.h>
 #endif
+#include<mpi.h>
 
-int main(void) {
+int main(int argc, char *argv[])
+{
+    int i, myid, ntasks;
 #ifdef _OPENACC
     acc_device_t devtype;
 #endif
 
-    printf("Hello world!\n");
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    if (myid == 0) {
+        printf("Number of MPI tasks: %d\n", ntasks);
+    }
+
+    printf("[%d] Hello world!\n", myid);
 #ifdef _OPENACC
     devtype = acc_get_device_type();
-    printf("Number of available OpenACC devices: %d\n",
-            acc_get_num_devices(devtype));
-    printf("Type of available OpenACC devices: %d\n", devtype);
+    printf("[%d] Number of available OpenACC devices: %d\n",
+            myid, acc_get_num_devices(devtype));
+    printf("[%d] Type of available OpenACC devices: %d\n", myid, devtype);
 #else
-    printf("Code compiled without OpenACC\n");
+    printf("[%d] Code compiled without OpenACC\n", myid);
 #endif
 
+    MPI_Finalize();
     return 0;
 }
