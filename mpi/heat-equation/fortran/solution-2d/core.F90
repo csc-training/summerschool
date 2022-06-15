bbd8b5eaa32f13bf8b35833e336cd2e51099c189
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2022-06-15 13:50:26 +0300

    Use sendrecv instead neighborhood

--- mpi/heat-equation/fortran/solution-2d/core.F90
+++ mpi/heat-equation/fortran/solution-2d/core.F90
@@ -11,39 +11,32 @@ contains
 
     type(field), intent(inout) :: field0
     type(parallel_data), intent(in) :: parallel
-
-    type(mpi_datatype) :: types(4)
-    integer :: counts(4) = [1, 1, 1, 1]
-    integer(kind=MPI_ADDRESS_KIND) :: sdisps(4), rdisps(4), disp0
-
-    integer :: i, ierr
-
-    types(1) = parallel%rowtype
-    types(2) = parallel%rowtype
-    types(3) = parallel%columntype
-    types(4) = parallel%columntype
-
-    ! calculate displacements with mpi_get_ddress    
-    call mpi_get_address(field0%data(0, 0), disp0)
-
-    call mpi_get_address(field0%data(1, 0), sdisps(1))
-    call mpi_get_address(field0%data(field0%nx, 0), sdisps(2))
-    call mpi_get_address(field0%data(0, 1), sdisps(3))
-    call mpi_get_address(field0%data(0, field0%ny), sdisps(4))
-
-    call mpi_get_address(field0%data(0, 0), rdisps(1))
-    call mpi_get_address(field0%data(field0%nx + 1, 0), rdisps(2))
-    call mpi_get_address(field0%data(0, 0), rdisps(3))
-    call mpi_get_address(field0%data(0, field0%ny + 1), rdisps(4))
-
-    do i = 1, 4
-       sdisps(i) = sdisps(i) - disp0
-       rdisps(i) = rdisps(i) - disp0
-    end do
     
-    call mpi_neighbor_alltoallw(field0%data, counts, sdisps, types, &
-                                field0%data, counts, rdisps, types, &
-                                parallel%comm, ierr)
+    integer :: ierr
+
+    ! Send to left, receive from right
+    call mpi_sendrecv(field0%data(0, 1), 1, parallel%columntype, &
+         & parallel%nleft, 11, &
+         & field0%data(0, field0%ny + 1), 1, parallel%columntype, &
+         & parallel%nright, 11, &
+         & parallel%comm, MPI_STATUS_IGNORE, ierr)
+
+    ! Send to right, receive from left
+    call mpi_sendrecv(field0%data(0, field0%ny), 1, parallel%columntype, &
+         & parallel%nright, 12, &
+         & field0%data(0, 0), 1, parallel%columntype, &
+         & parallel%nleft, 12, &
+         & parallel%comm, MPI_STATUS_IGNORE, ierr)
+
+    ! Send to up receive from down
+    call mpi_sendrecv(field0%data(1, 0), 1, parallel%rowtype, &
+         & parallel%nup, 13, field0%data(field0%nx+1, 0), 1, parallel%rowtype, &
+         & parallel%ndown, 13, parallel%comm, MPI_STATUS_IGNORE, ierr)
+
+    ! Send to the down, receive from up
+    call mpi_sendrecv(field0%data(field0%nx, 0), 1, parallel%rowtype, &
+         & parallel%ndown, 14, field0%data(0, 0), 1, parallel%rowtype, &
+         & parallel%nup, 14, parallel%comm, MPI_STATUS_IGNORE, ierr)
 
   end subroutine exchange
 
