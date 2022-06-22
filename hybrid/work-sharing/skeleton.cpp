81b34a4511a0567e5fd2296efaadab4e47aa0174
leopekkas <leopekkas@gmail.com>
2022-06-22 10:45:41 +0300

    Update hybrid exercises from C to C++

--- hybrid/work-sharing/skeleton.cpp
+++ hybrid/work-sharing/skeleton.cpp
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include <cstdio>
 
 #define NX 102400
 
 int main(void)
 {
     double vecA[NX], vecB[NX], vecC[NX];
-    double sum;
-    int i;
 
     /* Initialization of the vectors */
-    for (i = 0; i < NX; i++) {
+    for (int i = 0; i < NX; i++) {
         vecA[i] = 1.0 / ((double)(NX - i));
         vecB[i] = vecA[i] * vecA[i];
     }
@@ -19,9 +17,9 @@ int main(void)
      *   vecC = vecA + vecB
      */
 
-    sum = 0.0;
+    double sum = 0.0;
     /* Compute the check value */
-    for (i = 0; i < NX; i++) {
+    for (int i = 0; i < NX; i++) {
         sum += vecC[i];
     }
     printf("Reduction sum: %18.16f\n", sum);
