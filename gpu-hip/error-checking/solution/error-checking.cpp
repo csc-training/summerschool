110b9d9205ab0f90401259d038455e99ff3d8493
Jaro Hokkanen <jaro.hokkanen@gmail.com>
2022-06-22 17:23:36 +0300

    Add HIP error check exercise

---
+++ gpu-hip/error-checking/solution/error-checking.cpp
@@ -0,0 +1,24 @@
+#include <hip/hip_runtime.h>
+#include <stdio.h>
+
+/* HIP error handling macro */
+#define HIP_ERRCHK(err) (hip_errchk(err, __FILE__, __LINE__ ))
+static inline void hip_errchk(hipError_t err, const char *file, int line) {
+    if (err != hipSuccess) {
+        printf("\n\n%s in %s at line %d\n", hipGetErrorString(err), file, line);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(void)
+{
+    int count, device;
+
+    HIP_ERRCHK( hipGetDeviceCount(&count) );
+    HIP_ERRCHK( hipSetDevice(count - 0) );
+    HIP_ERRCHK( hipGetDevice(&device) );
+
+    printf("Hello! I'm GPU %d out of %d GPUs in total.\n", device, count);
+
+    return 0;
+}
