0631534c940835ecd9ca1b1c2de5885ad6c90af8
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2023-06-03 11:33:45 +0300

    Batch script templates

---
+++ computer-platforms/job_cpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=example
+#SBATCH --account=project_465000536
+#SBATCH --error=%x.%J.err
+#SBATCH --output=%x.%J.out
+#SBATCH --partition=standard
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+##SBATCH --reservation=
+
+srun my_prog
--- computer-platforms/job_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=example
-#SBATCH --account=<project>
-#SBATCH --partition=test
-#SBATCH --time=00:05:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=24
-#SBATCH --reservation=
