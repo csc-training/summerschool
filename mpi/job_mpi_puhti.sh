b03d5d5b3f3293b049c91891641367ea12646456
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-06-06 16:45:51 +0300

    Remove old job script

--- mpi/job_mpi_puhti.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=example
-#SBATCH --account=project_2000745
-#SBATCH --partition=large
-#SBATCH --reservation=summerschool
-#SBATCH --time=00:05:00
-#SBATCH --ntasks=4
-
-srun my_mpi_exe
