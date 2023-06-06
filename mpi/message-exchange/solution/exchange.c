5e4c13143a4d2d1267ddaf2116cc2f8f2f545fe7
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-06-06 17:33:41 +0300

    Make C version similar to C++ version

--- mpi/message-exchange/solution/exchange.c
+++ mpi/message-exchange/solution/exchange.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<mpi.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    int i, myid, ntasks;
+    int myid, ntasks, nrecv;
+    int arraysize = 100000;
     int msgsize = 100;
     int *message;
     int *receiveBuffer;
@@ -15,29 +15,35 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-    /* Allocate message */
-    message = (int *)malloc(sizeof(int) * msgsize);
-    receiveBuffer = (int *)malloc(sizeof(int) * msgsize);
-    /* Initialize message */
-    for (i = 0; i < msgsize; i++) {
+    // Allocate buffers
+    message = (int *)malloc(sizeof(int) * arraysize);
+    receiveBuffer = (int *)malloc(sizeof(int) * arraysize);
+
+    // Initialize message and receive buffer
+    for (int i = 0; i < arraysize; i++) {
         message[i] = myid;
+        receiveBuffer[i] = -1;
     }
 
-    /* Send and receive messages as defined in exercise */
+    // Send and receive messages as defined in exercise
     if (myid == 0) {
         MPI_Send(message, msgsize, MPI_INT, 1, 1, MPI_COMM_WORLD);
         MPI_Recv(receiveBuffer, msgsize, MPI_INT, 1, 2, MPI_COMM_WORLD,
                  &status);
-        printf("Rank %i received %i\n", myid, receiveBuffer[0]);
+        MPI_Get_count(&status, MPI_INT, &nrecv);
+        printf("Rank %i received %i elements, first %i\n", myid, nrecv, receiveBuffer[0]);
     } else if (myid == 1) {
         MPI_Send(message, msgsize, MPI_INT, 0, 2, MPI_COMM_WORLD);
         MPI_Recv(receiveBuffer, msgsize, MPI_INT, 0, 1, MPI_COMM_WORLD,
                  &status);
-        printf("Rank %i received %i\n", myid, receiveBuffer[0]);
+        MPI_Get_count(&status, MPI_INT, &nrecv);
+        printf("Rank %i received %i elements, first %i\n", myid, nrecv, receiveBuffer[0]);
     }
 
+    // Free buffers
     free(message);
     free(receiveBuffer);
+
     MPI_Finalize();
     return 0;
 }
