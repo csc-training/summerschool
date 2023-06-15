bf04d8912e66a87b9c98caa5c3377572bdf87fd8
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-06-15 12:13:05 +0300

    Rename my_id to myid

--- mpi/cartesian-grid/solution/cartesian-grid.c
+++ mpi/cartesian-grid/solution/cartesian-grid.c
@@ -4,7 +4,7 @@
 
 
 int main(int argc, char* argv[]) {
-    int ntasks, my_id, irank;
+    int ntasks, myid, irank;
     int dims[2] = {0};      /* Dimensions of the grid */
     int coords[2] = {0};    /* Coordinates in the grid */
     int neighbors[4] = {0}; /* Neighbors in 2D grid */
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
     /* Determine the process grid (dims[0] x dims[1] = ntasks) */
     if (ntasks < 16) {
@@ -42,12 +42,12 @@ int main(int argc, char* argv[]) {
     MPI_Cart_shift(comm2d, 1, 1, &neighbors[2], &neighbors[3]);
 
     /* Find out and store also the Cartesian coordinates of a rank */
-    MPI_Cart_coords(comm2d, my_id, 2, coords);
+    MPI_Cart_coords(comm2d, myid, 2, coords);
 
     for (irank = 0; irank < ntasks; irank++) {
-        if (my_id == irank) {
+        if (myid == irank) {
             printf("%3i = %2i %2i neighbors=%3i %3i %3i %3i\n",
-                   my_id, coords[0], coords[1], neighbors[0], neighbors[1],
+                   myid, coords[0], coords[1], neighbors[0], neighbors[1],
                    neighbors[2], neighbors[3]);
         }
         MPI_Barrier(MPI_COMM_WORLD);
