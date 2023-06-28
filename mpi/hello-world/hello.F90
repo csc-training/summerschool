7211fefc24037c99d41f4266fff77077025c5ef9
hongdinggao <hongdinggao@gmail.com>
2023-06-28 11:13:16 +0300

    first try

--- mpi/hello-world/hello.F90
+++ mpi/hello-world/hello.F90
@@ -1,7 +1,10 @@
 program hello
+  use mpi_f08
   implicit none
-
+  integer :: ierror
+  call MPI_INIT(ierror)
   ! TODO: say hello! in parallel
   write(*,*) 'Hello!'
+  call MPI_FINALIZE(ierror)
 
 end program hello
