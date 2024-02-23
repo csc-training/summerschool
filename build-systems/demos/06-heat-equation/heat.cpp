d2f81b6f66a9850c0ffb3a3870288475c565f087
Juhani Kataja <juhani.kataja@csc.fi>
2024-02-23 16:55:36 +0200

    Add a lecture build-systems/docs/01-cmake Add demos 6 under build-systems/demos/

---
+++ build-systems/demos/06-heat-equation/heat.cpp
@@ -0,0 +1,51 @@
+#include "heat.hpp"
+#include "matrix.hpp"
+#include <iostream>
+
+void Field::setup(int nx_in, int ny_in) 
+{
+    nx_full = nx_in;
+    ny_full = ny_in;
+
+    nx = nx_full;
+    ny = ny_full;
+
+   // matrix includes also ghost layers
+   temperature = Matrix<double> (nx + 2, ny + 2);
+}
+
+void Field::generate() {
+
+    // Radius of the source disc 
+    auto radius = nx_full / 6.0;
+    #pragma omp parallel for
+    for (int i = 0; i < nx + 2; i++) {
+        for (int j = 0; j < ny + 2; j++) {
+            // Distance of point i, j from the origin 
+            auto dx = i - nx / 2 + 1;
+            auto dy = j - ny / 2 + 1;
+            if (dx * dx + dy * dy < radius * radius) {
+                temperature(i, j) = 5.0;
+            } else {
+                temperature(i, j) = 65.0;
+            }
+        }
+    }
+
+    // Boundary conditions
+    #pragma omp parallel for
+    for (int i = 0; i < nx + 2; i++) {
+        // Left
+        temperature(i, 0) = 20.0;
+        // Right
+        temperature(i, ny + 1) = 70.0;
+    }
+
+    #pragma omp parallel for
+    for (int j = 0; j < ny + 2; j++) {
+        // Top
+        temperature(0, j) = 85.0;
+        // Bottom
+        temperature(nx + 1, j) = 5.0;
+        }    
+}
