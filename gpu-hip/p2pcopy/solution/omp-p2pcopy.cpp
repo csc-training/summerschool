16cd52676664d9cf572a0204e6e9899289a73d22
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-07-02 09:23:55 +0300

    Fix exercise

--- gpu-hip/p2pcopy/solution/omp-p2pcopy.cpp
+++ gpu-hip/p2pcopy/solution/omp-p2pcopy.cpp
@@ -1,28 +1,25 @@
-
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
+#include <chrono>
+#include <cstdio>
+#include <cmath>
 #include <omp.h>
 
 
-void copyP2P(int gpu0, int gpu1, int* dA_0, int* dA_1, int size) {
+void copyP2P(int gpu0, int gpu1, int* dA_0, int* dA_1, int N) {
 
     // Do a dummy copy without timing to remove the impact of the first one
-    omp_target_memcpy(dA_0, dA_1, size, 0, 0, gpu0, gpu1);
+    omp_target_memcpy(dA_0, dA_1, N, 0, 0, gpu0, gpu1);
 
     // Do a series of timed P2P memory copies
-    int N = 10;
-    clock_t tStart = clock();
-    for (int i = 0; i < N; ++i) {
-      omp_target_memcpy(dA_0, dA_1, size, 0, 0, gpu0, gpu1);
+    int M = 10;
+    auto tStart = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < M; ++i) {
+        omp_target_memcpy(dA_0, dA_1, N, 0, 0, gpu0, gpu1);
     }
-    clock_t tStop = clock();
+    auto tStop = std::chrono::high_resolution_clock::now();
 
     // Calcute time and bandwith
-    double time_s = (double) (tStop - tStart) / CLOCKS_PER_SEC;
-    double bandwidth = (double) size * (double) N / (double) 1e9 / time_s;
+    double time_s = std::chrono::duration_cast<std::chrono::nanoseconds>(tStop - tStart).count() / 1e9;
+    double bandwidth = (double) N * sizeof(int) / (1024*1024*1024) / (time_s / M);
     printf("P2P, Bandwith: %.3f (GB/s), Time: %.3f s\n", bandwidth, time_s);
 }
 
@@ -35,18 +32,18 @@ int main(int argc, char *argv[])
         printf("Need at least two GPUs!\n");
         exit(EXIT_FAILURE);
     } else {
-        printf("Found %d GPU devices, using GPUs 0 and 1!\n", devcount);
+        printf("Found %d GPU devices\n", devcount);
     }
 
     // Allocate memory for both GPUs
-    int size = pow(2, 28);
+    int N = pow(2, 28);
     int gpu0 = 0, gpu1 = 1;
     int *dA_0, *dA_1;
-    dA_0 = (int*)omp_target_alloc(size, gpu0);
-    dA_1 = (int*)omp_target_alloc(size, gpu1);
+    dA_0 = (int*)omp_target_alloc(N * sizeof(int), gpu0);
+    dA_1 = (int*)omp_target_alloc(N * sizeof(int), gpu1);
 
     // Memcopy, P2P
-    copyP2P(gpu0, gpu1, dA_0, dA_1, size);
+    copyP2P(gpu0, gpu1, dA_0, dA_1, N);
 
     // Deallocate device memory
     omp_target_free(dA_0, gpu0);
