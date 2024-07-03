db08cf755abc37fb6d393665d370e3056bcba3b2
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-07-03 10:22:36 +0300

    Add tdd demo

---
+++ application-testing/demos/tdd_test/tests/unit_tests/field_tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(field_test, domain_partition_succeeds) {
+    constexpr int num_rows = 100;
+    constexpr int num_cols = 100;
+    constexpr int num_partitions = 10;
+    ASSERT_TRUE(true) << "Not true";
+}
