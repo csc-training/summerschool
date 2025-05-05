632a7ae31c3d9e4aa6e444a9ab6f5a5d54fd05c2
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-05-05 15:32:58 +0300

    Wrap function call with a lambda

--- application-performance/demos/idle_resources/common.h
+++ application-performance/demos/idle_resources/common.h
@@ -25,29 +25,30 @@
 #if defined(RUN_ON_THE_DEVICE)
 #include <hip/hip_runtime.h>
 
-template <typename T>
-__global__ void kernel(size_t n, T t, void (*f)(T &, size_t)) {
+template <typename T, typename Lambda>
+__global__ void kernel(size_t n, T t, Lambda lambda) {
     const size_t tid = threadIdx.x + blockIdx.x * blockDim.x;
     const size_t stride = blockDim.x * gridDim.x;
 
     for (size_t i = tid; i < n; i += stride) {
-        f(t, i);
+        lambda(t, i);
     }
 }
 
 #endif
 
-template <typename T> void loop(size_t n, T &t, void (*f)(T &, size_t)) {
+template <typename T, typename Lambda>
+void loop(size_t n, T &t, Lambda lambda) {
 #if defined(RUN_ON_THE_DEVICE)
     static constexpr dim3 blocks(1024);
     static constexpr dim3 threads(1024);
-    kernel<T><<<threads, blocks>>>(n, t, f);
+    kernel<<<threads, blocks>>>(n, t, lambda);
     [[maybe_unused]] const auto result = hipDeviceSynchronize();
 #else
     // clang-format off
     #pragma omp parallel for
     for (size_t i = 0; i < n; i++) {
-        f(t, i);
+        lambda(t, i);
     }
     // clang-format on
 #endif
@@ -79,14 +80,14 @@ template <typename T, typename... Args> void run_and_measure(Args... args) {
     constexpr size_t max_n = *std::max_element(ns.begin(), ns.end());
 
     T t(max_n, allocate, deallocate, args...);
-    loop(max_n, t, T::init);
+    loop(max_n, t, [](T &t, size_t i) { T::init(t, i); });
 
     for (size_t n : ns) {
         constexpr auto n_iter = 20;
         size_t avg = 0;
         for (auto iteration = 0; iteration < n_iter; iteration++) {
             const auto start = std::chrono::high_resolution_clock::now();
-            loop(n, t, T::compute);
+            loop(max_n, t, [](T &t, size_t i) { T::compute(t, i); });
             const auto end = std::chrono::high_resolution_clock::now();
             const std::chrono::duration<double, std::nano> dur = end - start;
             avg += iteration == 0 ? 0 : dur.count();
