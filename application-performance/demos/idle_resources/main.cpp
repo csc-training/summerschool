2b3c8178184abff1144bfbffd37dbd564a000f87
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-05-06 13:21:22 +0300

    Add input parsing

--- application-performance/demos/idle_resources/main.cpp
+++ application-performance/demos/idle_resources/main.cpp
@@ -1,11 +1,78 @@
 #include "common.h"
+#include <array>
+#include <charconv>
+#include <iostream>
+#include <string_view>
 
-// #include "axpy.h"
+#include "axpy.h"
 #include "taylor.h"
 
-int main(int, char **) {
-    // run_and_measure<Axpy<float>>(2.216f);
-    run_and_measure<Taylor<float>>(-10.0f, 10.0);
+void handle_input(int argc, char **argv) {
+    static constexpr std::array<std::string_view, 2> arguments = {
+        "axpy a",
+        "taylor range_min range_max num_iters",
+    };
+
+    if (argc <= 1) {
+        std::cerr << "Usage:";
+        for (size_t i = 0; i < arguments.size() - 1; i++) {
+            std::cerr << "\n\t" << argv[0] << " " << arguments[i] << " or";
+        }
+        std::cerr << "\n\t" << argv[0] << " " << arguments[arguments.size() - 1]
+                  << std::endl;
+    } else {
+        std::string input(argv[1]);
+        auto n = 0;
+        for (auto &arg : arguments) {
+            auto substr = arg.substr(0, arg.find(' '));
+            if (0 == input.compare(substr)) {
+                auto get_arg = [](const char *arg, auto &value) {
+                    auto [ptr, ec] =
+                        std::from_chars(arg, arg + sizeof(arg), value);
+                    assert(ec == std::errc() && "Failed to read argument");
+                };
+
+                switch (n) {
+                case 0: {
+                    assert(argc == 3 && "Incorrect amount of arguments");
+                    float a = 0.0f;
+                    get_arg(argv[2], a);
+                    std::cerr << "Running axpy with argument " << a
+                              << std::endl;
+                    run_and_measure<Axpy<float>>(a);
+                    break;
+                }
+                case 1: {
+                    assert(argc == 5 && "Incorrect amount of arguments");
+                    float min = 0;
+                    float max = 0;
+                    size_t num_iters = 0;
+
+                    get_arg(argv[2], min);
+                    get_arg(argv[3], max);
+                    get_arg(argv[4], num_iters);
+
+                    std::cerr << "Running taylor with arguments " << min << ", "
+                              << max << ", " << num_iters << std::endl;
+                    run_and_measure<Taylor<float>>(min, max, num_iters);
+                    break;
+                }
+                default:
+                    std::cerr << "Index out of range" << std::endl;
+                    break;
+                }
+                return;
+            }
+            n++;
+        }
+    }
+
+    std::cerr << "Given subprogram name \"" << argv[1] << "\" didn't match"
+              << std::endl;
+}
+
+int main(int argc, char **argv) {
+    handle_input(argc, argv);
 
     return 0;
 }
--- application-performance/demos/idle_resources/taylor.h
+++ application-performance/demos/idle_resources/taylor.h
@@ -6,19 +6,23 @@
 #include <cstddef>
 
 template <typename T> struct Taylor {
+    // This computes the Taylor's expansion for e^x,
+    // with x in [minimum, maximum]
+
     T *x = nullptr;
     T *r = nullptr;
     void (*deallocate)(void *);
     const T minimum;
     const T maximum;
     const size_t size;
+    const size_t num_iters;
 
     Taylor(size_t size, void *(*allocate)(size_t), void (*deallocate)(void *),
-           T minimum, T maximum)
+           T minimum, T maximum, size_t num_iters)
         : x(static_cast<T *>(allocate(sizeof(T) * size))),
           r(static_cast<T *>(allocate(sizeof(T) * size))),
           deallocate(deallocate), minimum(minimum), maximum(maximum),
-          size(size) {
+          size(size), num_iters(num_iters) {
         assert(maximum > minimum);
     }
 
@@ -29,20 +33,21 @@ template <typename T> struct Taylor {
 
     DEVICE void init(size_t i) {
         const T width = maximum - minimum;
-        x[i] = i / size * width;
+        x[i] = minimum + i / size * width;
         r[i] = 0.0;
     }
 
     DEVICE void compute(size_t i) {
         const T xi = x[i];
-        // clang-format off
-        r[i] = 1.0 +
-               xi +
-               xi * xi / 2.0 +
-               xi * xi * xi / 6.0 +
-               xi * xi * xi * xi / 24.0 +
-               xi * xi * xi * xi * xi / 120.0 +
-               xi * xi * xi * xi * xi * xi / 720.0;
-        // clang-format on
+        T sum = 1.0;
+        T xe = 1.0;
+        T factorial = 1.0;
+
+        for (size_t j = 1; j < num_iters; j++) {
+            xe *= xi;
+            factorial *= static_cast<T>(j);
+            sum += xe / factorial;
+        }
+        r[i] = sum;
     }
 };
