d14cf26d89f69bf291b2a6745558e099c8cfaafe
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-05-14 15:26:24 +0300

    Update exercise

--- gpu/exercises/fundamentals/02-kernels/02_kernel_print/README.md
+++ gpu/exercises/fundamentals/02-kernels/02_kernel_print/README.md
@@ -5,3 +5,8 @@ Next, change
 - the number of blocks
 
 and recompile & run to see what the output looks like.
+
+Use smallish numbers for threads & blocks to make some sense of the output.
+
+Finally, change `num_threads` to 1025 and see what happens.
+Then move on to the next exercise to find out more.
--- gpu/exercises/fundamentals/02-kernels/02_kernel_print/main.cpp
+++ gpu/exercises/fundamentals/02-kernels/02_kernel_print/main.cpp
@@ -1,14 +1,19 @@
 #include <hip/hip_runtime.h>
 
-__global__ void hello() {
-    printf("Hello world, this is kernel speaking!\n");
+__global__ void hello(int32_t num_blocks, int32_t num_threads) {
+    printf("Hello world, this is kernel speaking!\n %d * %d = %d threads are "
+           "printing this message\n",
+           num_blocks, num_threads, num_blocks * num_threads);
 }
 
 int main() {
+    // TODO: See how the output changes when you change these to different
+    // numbers. Once you've experimented with some numbers like 2, 8, 16, try
+    // setting num_threads to 1025. What happens?
     const int32_t num_blocks = 1;
     const int32_t num_threads = 1;
 
-    hello<<<num_blocks, num_threads>>>();
+    hello<<<num_blocks, num_threads>>>(num_blocks, num_threads);
 
     return 0;
 }
