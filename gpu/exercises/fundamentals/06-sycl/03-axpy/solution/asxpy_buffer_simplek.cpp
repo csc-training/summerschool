9d343a7bba5eb7529f7e45fb025e4c8335e4228f
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2025-05-30 15:52:23 +0300

    Update asxpy_buffer_simplek.cpp

--- gpu/exercises/fundamentals/06-sycl/03-axpy/solution/asxpy_buffer_simplek.cpp
+++ gpu/exercises/fundamentals/06-sycl/03-axpy/solution/asxpy_buffer_simplek.cpp
@@ -5,20 +5,20 @@ using namespace sycl;
 int main() {
   // Set up queue on any available device
   queue q;
-
+  
   // Initialize input and output memory on the host
   constexpr size_t N = 25600;
   std::vector<int> x(N),y(N);
   int a=4;
   std::fill(x.begin(), x.end(), 1);
   std::fill(y.begin(), y.end(), 2);
-
+  
   {
-   // Create buffers for the host data
+    // Create buffers for the host data
     buffer<int> x_buf(x.data(), range<1>(N));
     buffer<int> y_buf(y.data(), range<1>(N));
-
-// Submit the kernel to the queue
+    
+    // Submit the kernel to the queue
     q.submit([&](handler& h) {
       // Create accessors
       accessor x_acc(x_buf, h, read_only);
@@ -31,14 +31,14 @@ int main() {
     //Checking the result inside the scope of the buffers using host_accessors
     host_accessor y_acc(y_buf, read_only);
     bool passed = std::all_of(y_acc.begin(), y_acc.end(),
-                              [a](int val) { return val == 1 + a * 2; });
+                              [a](int val) { return val == a * 1 + 2; });
     std::cout << (passed ? "SUCCESS" : "FAILURE") << std::endl;
     return passed ? 0 : 1;
   }
 
   // Check that all outputs match expected value
   bool passed = std::all_of(y.begin(), y.end(),
-                            [a](int val) { return (val == 1+a*2); });
+                            [a](int val) { return val == a * 1 + 2; });
   std::cout << ((passed) ? "SUCCESS" : "FAILURE")
             << std::endl;
   return (passed) ? 0 : 1;
