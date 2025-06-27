084a8330e8a5b0ad3941eecb14af33fd45b6ce72
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-06-27 09:16:16 +0300

    Update code

--- openmp/multiple-thread-communication/solution/multiple.cpp
+++ openmp/multiple-thread-communication/solution/multiple.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     int provided, rank, ntasks;
-    int tid, nthreads, msg, i;
+    int tid, nthreads;
 
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
 
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
 
-#pragma omp parallel private(msg, tid, nthreads, i)
+#pragma omp parallel private(tid, nthreads)
 {
     nthreads = omp_get_num_threads();
     tid = omp_get_thread_num();
@@ -29,15 +29,15 @@ int main(int argc, char *argv[])
     if (rank == 0) {
 #pragma omp single
         {
-            printf("%i threads in master rank\n", nthreads);
+            printf("%d threads in master rank\n", nthreads);
         }
-        for (i = 1; i < ntasks; i++) {
-            MPI_Send(&tid, 1, MPI_INTEGER, i, tid, MPI_COMM_WORLD);
+        for (int i = 1; i < ntasks; i++) {
+            MPI_Send(&tid, 1, MPI_INT, i, tid, MPI_COMM_WORLD);
         }
     } else {
-        MPI_Recv(&msg, 1, MPI_INTEGER, 0, tid, MPI_COMM_WORLD,
-                MPI_STATUS_IGNORE);
-        printf("Rank %i thread %i received %i\n", rank, tid, msg);
+        int msg;
+        MPI_Recv(&msg, 1, MPI_INT, 0, tid, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Rank %d thread %d received %d\n", rank, tid, msg);
     }
 }
 
