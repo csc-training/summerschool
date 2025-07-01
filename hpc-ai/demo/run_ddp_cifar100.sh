abc131975a49a955aa793a56b7f2cb542753de24
hsnfirooz <hsnfirooz@gmail.com>
2025-07-01 15:40:38 +0300

    fix OMP threads

--- hpc-ai/demo/run_ddp_cifar100.sh
+++ hpc-ai/demo/run_ddp_cifar100.sh
@@ -15,7 +15,7 @@ module load singularity-AI-bindings
 SIF=/scratch/project_462000956/resources/lumi-pytorch-rocm-6.2.1-python-3.12-pytorch-20240918-vllm-4075b35.sif
 export SINGULARITYENV_PREPEND_PATH=/user-software/bin
 
-srun singularity exec $SIF bash -c \
+OMP_NUM_THREADS=1 srun singularity exec $SIF bash -c \
     '$WITH_CONDA; \
      source /scratch/project_462000956/resources/hpc-ai/bin/activate; \
      python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=2 train_ddp_cifar100.py'
